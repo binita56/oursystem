bf09529e0dd36a27da0c4bc1bb1d291a06abd8be
Saroj Dahal <saroj.dahal.1213@gmail.com>
2017-08-01 16:03:56 +0545

    I have done changes in url given to the register by creating methods in controller

---
+++ application/views/loginform.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Title Page</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		
+
+		<div class="maindiv col-md-4" style="margin-top:300px;text-align:center;">
+
+<form name="myForm" action="check.php" method="post">
+
+		<div class="panel panel-default">
+		  <div class="panel-heading">Login/Register!</div>
+		  <div class="panel-body">
+		   
+<!--//Panel content-->
+				   <div class="input-group">
+					  <span class="input-group-addon" id="basic-addon1">Username @</span>
+					  <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">	 
+					</div>
+		 			<br>
+
+		 			<div class="input-group">
+		 			<span class="input-group-addon" id="basic-addon1">password * *</span>
+					  <input type="password" name="password" id="inputPassword" class="form-control" required="required" placeholder="Password"  title="" aria-describedby="basic-addon1">
+					  
+				</div>
+			<br>
+					  <button type="submit" class="btn btn-primary">Login</button>
+					  <a href="welcome/register"><button type="button" class="btn btn-danger">Register</button></a>
+
+<!--end of panel content-->
+		  </div>
+		</div>
+
+			</form>
+ 		 </div>
+
+
+		
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
